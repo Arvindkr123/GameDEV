230f4c431d4a9bd1e4ee77141704ac333544d337
= <=>
2025-07-27 15:47:16 +0530

    done with horizontanl movement and vertical movement with sin wave

--- NPC_movement_animations/app1.js
+++ NPC_movement_animations/app1.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 123;
+const numberOfEnemies = 30;
 const enemiesArray = []
 let gameFrame = 0;
 
@@ -12,23 +12,31 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = "./enemies/enemy1.png";
-        // this.speed = Math.random() * 4 - 2
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
+        // this.image.src = "./enemies/enemy1.png";
+        this.image.src = "./enemies/enemy2.png";
+        this.speed = Math.random() * 4 - 1
+        // this.spriteWidth = 293;
+        // this.spriteHeight = 155;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.5;
+        this.curve = Math.random()*20;
     }
 
     update() {
-        // this.x += this.speed;
-        this.x += Math.random() * 15 - 2.5;
-        this.y += Math.random() * 10 - 2.5;
-        // this.y += this.speed;
+        this.x -= this.speed;
+        this.y += this.curve* Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if(this.x + this.width < 0){
+            this.x = canvas.width;
+        }
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++
         }
