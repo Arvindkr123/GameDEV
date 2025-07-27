d1d8054a97bdb88777b990208fe8c6b30be9b9a4
= <=>
2025-07-27 15:28:55 +0530

    done pattern 1

--- NPC_movement_animations/app1.js
+++ NPC_movement_animations/app1.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 10;
+const numberOfEnemies = 123;
 const enemiesArray = []
 let gameFrame = 0;
 
@@ -13,24 +13,26 @@ class Enemy {
     constructor() {
         this.image = new Image();
         this.image.src = "./enemies/enemy1.png";
-        this.speed = Math.random() * 4 - 2
+        // this.speed = Math.random() * 4 - 2
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
-        this.x = Math.random() * (canvas.width-this.width);
-        this.y = Math.random() * (canvas.height-this.height);
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
     }
 
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        // this.x += this.speed;
+        this.x += Math.random() * 15 - 2.5;
+        this.y += Math.random() * 10 - 2.5;
+        // this.y += this.speed;
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++
         }
-    }   
+    }
 
     draw() {
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
