3152f94ed4f00877e4c97719a7642bb2a631eb2c
= <=>
2025-07-27 15:12:31 +0530

    envemy movement pattern 1 complete

--- NPC_movement_animations/app.js
+++ NPC_movement_animations/app.js
@@ -4,17 +4,17 @@ const ctx = canvas.getContext('2d');
 
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 110;
+const numberOfEnemies = 30;
 const enemiesArray = []
 let gameFrame = 0;
 
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = "./enemies/enemy2.png";
+        this.image.src = "./enemies/enemy3.png";
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2;
         this.height = this.spriteHeight / 2;
         this.x = Math.random() * (canvas.width - this.width);
@@ -22,14 +22,14 @@ class Enemy {
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 5 + 1; 
+        this.angleSpeed = Math.random() * 2 + 0.5;
+        this.curve = Math.random() * 200 + 50;
     }
 
     update() {
-        this.x -= this.speed;
+        this.x = this.curve * Math.sin(this.angle * Math.PI / 180) + (canvas.width / 2 - this.width / 2);
+        this.y = this.curve * Math.cos(this.angle * Math.PI / 100) + (canvas.height / 2 - this.height / 2);
         if (this.x + this.width < 0) this.x = canvas.width
-        this.y += this.curve * Math.sin(this.angle);
         this.angle += this.angleSpeed;
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
@@ -38,7 +38,7 @@ class Enemy {
 
     draw() {
         // ctx.fillRect(this.x, this.y, this.width, this.height);
-        //ctx.strokeRect(this.x, this.y, this.width, this.height);
+        // ctx.strokeRect(this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
@@ -53,7 +53,7 @@ function animate() {
         enemy.update();
         enemy.draw();
     })
-    gameFrame++;
+    gameFrame += 1;
     requestAnimationFrame(animate);
 }
 
